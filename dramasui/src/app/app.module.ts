80d9334ae1893f175574ee7fdd155187feefb9df
buriak.dzmitry <dzmitry.buriak@leverx.com>
2018-01-14 21:37:32 +0300

    Add initial screen components

--- dramasui/src/app/app.module.ts
+++ dramasui/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderMenuComponent} from './header-menu/header-menu.component';
+import {SectionMenuComponent} from './section-menu/section-menu.component';
+import {SliderComponent} from './slider/slider.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    SectionMenuComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +22,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
